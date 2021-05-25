9061fd86268d9e63fe3d56f289d9bd31b18ef292
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 20:54:35 +0530

    Add files via upload

---
+++ Pattern6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pattern6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write(" " + j + " ");
+                    }
+                    else 
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
