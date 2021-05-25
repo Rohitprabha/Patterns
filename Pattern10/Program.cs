6ac7c4b34b13d7b5d5847d19051bec0b30088642
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 21:01:41 +0530

    Add files via upload

---
+++ Pattern10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pattern10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = n; j >= 1; j--)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
