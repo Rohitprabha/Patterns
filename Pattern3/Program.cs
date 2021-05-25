10c3aa44a6ee1ab2d5d0db74352738ce7455515a
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 20:28:08 +0530

    Add files via upload

---
+++ Pattern3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pattern3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n - 1; j >= i; j--)
+                {
+                    Console.Write("   ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(" " + i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                n = n - 1;
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (j <= n)
+                    {
+                        Console.Write("   ");
+                    }
+                    else
+                    {
+                        Console.Write(" " + i + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
