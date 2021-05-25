efb7c897c878ef3637455b5e3fc0e036aacdeee1
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 20:34:40 +0530

    Add files via upload

---
+++ Pattern5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pattern5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = n; j >= i; j--)
+                {
+                    Console.Write(" " + j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
