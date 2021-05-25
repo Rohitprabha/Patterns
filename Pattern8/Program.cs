45e4a9e6b1d1a4f72cfae2ebb20433ea56eee3d7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 20:57:37 +0530

    Add files via upload

---
+++ Pattern8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pattern8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = n; j >= 1; j--)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
